aa11d537ca693863ded75de3860b6bb2b96afcd0
Aitalina Kirillina <ayta1951@gmail.com>
2020-12-11 11:33:41 +0100

    commit number 2

--- deel-2.js
+++ deel-2.js
@@ -10,16 +10,16 @@
  
 
 //B
-  doubleArrayValues = [1, 2, 3];
+doubleArrayValues = [1, 2, 3];
 
-  const doubledArray = doubleArrayValues.map(x => x * 2);
+const doubledArray = doubleArrayValues.map(x => x * 2);
 
-  console.log(doubledArray)
+console.log(doubledArray)
 
 
 //C
 const array1 = [1, 4, 3, 6, 9, 7, 11];
-const array2 = [1,2,1,2,1,2];
+const array2 = [1, 2, 1, 2, 1, 2];
 
 const containsNumberBiggerThan10 = array1.some(x => x > 10);
 
@@ -30,7 +30,7 @@ console.log(containsNumberBiggerThan10)
 
 
 //D
-const TheGreat7 = ['Canada', 'France','Germany','Italy','Japan','United Kingdom','United States'];
+const TheGreat7 = ['Canada', 'France', 'Germany', 'Italy', 'Japan', 'United Kingdom', 'United States'];
 
 const isItalyInTheGreat7 = TheGreat7.some(x => x == 'Italy');
 
@@ -38,23 +38,23 @@ console.log(isItalyInTheGreat7)
 
 
 //E
-   const tenfold = function () { 
-        
-        const integers = [ 1, 4, 3, 6, 9, 7, 11 ]; 
-        const copy = []; 
-  
-        integers.forEach(function (integer) { 
-            copy.push(integer * 10); 
-        }); 
-  
-       console.log(copy); 
-    } 
-    tenfold(); 
+const tenfold = function () {
+
+    const integers = [1, 4, 3, 6, 9, 7, 11];
+    const copy = [];
+
+    integers.forEach(function (integer) {
+        copy.push(integer * 10);
+    });
+
+    console.log(copy);
+}
+tenfold();
 
 
 
 //F
-const numbers = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98 ];
+const numbers = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98];
 
 const isBelow100 = numbers.every(x => x < 100);
 
@@ -65,6 +65,6 @@ const numbers = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98, 101, 206,
 
 const bigSum = numbers.reduce((total, num) => total + num);
 
-  console.log(bigSum)
+console.log(bigSum)
 
   
